e6e1f69ca7f5d3d9725cb58d4fbbd1c2bcbd34cb
bcjaeger <byron.jaeger@gmail.com>
2024-06-28 11:20:40 -0400

    version fix

--- R/aorsf_select_vars.R
+++ R/aorsf_select_vars.R
@@ -11,6 +11,6 @@ aorsf_select_vars <- function(data, formula, ...){
   fit <- orsf(data = data, formula = formula, ...)
   vars <- orsf_vs(fit, n_predictor_min = 1)
   top <- which.max(vars$stat_value)
-  c(fit$get_names_y(), vars$variables_included[top][[1]])
+  c("apoe4", vars$variables_included[top][[1]])
 
 }
