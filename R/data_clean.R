84dc6e9c96f0debe8ac42e00ecf9d2bf0d62e68c
bcjaeger <byron.jaeger@gmail.com>
2024-07-07 14:02:50 -0400

    allow time_var
    in case the time variable isn't named 'time' at this stage of data preparation.

--- R/data_clean.R
+++ R/data_clean.R
@@ -11,38 +11,38 @@
 #'
 
 
-data_clean <- function(data){
+data_clean <- function(data, time_var){
 
   UseMethod("data_clean")
 
 }
 
-data_clean.melodem_data <- function(data){
+data_clean.melodem_data <- function(data, time_var){
 
-  data$values %<>% data_clean_minimal()
+  data$values %<>% data_clean_minimal(time_var)
 
   data
 
 }
 
 
-data_clean_minimal <- function(data){
+data_clean_minimal <- function(data, time_var){
 
   data_out <- data
 
   # be picky about negative times but let time values of 0 go by
 
-  if(any(data_out$time < 0 )){
+  if(any(data_out[[time_var]] < 0 )){
     stop('time column contains negative values but all values should be > 0',
          call. = FALSE)
   }
 
-  if(any(data_out$time == 0)){
+  if(any(data_out[[time_var]] == 0)){
 
     warning("time column contains 0's - these will be replaced by 0.01",
             call. = FALSE)
 
-    data_out$time %<>% pmin(0.01)
+    data_out[[time_var]] %<>% pmax(0.01)
 
   }
 
@@ -51,9 +51,7 @@ data_clean_minimal <- function(data){
 }
 
 
-data_clean.melodem_sim <- function(data){
-
-  data$values %<>% data_clean_minimal()
+data_clean.melodem_sim <- function(data, time_var){
 
   dt <- as.data.table(data$values)
 
