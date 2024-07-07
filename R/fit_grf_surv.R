1ab8feff6ef60e910efc633605c056e2071414d4
bcjaeger <byron.jaeger@gmail.com>
2024-07-07 14:06:06 -0400

    allow use of propensity score or randomized treatment

--- R/fit_grf_surv.R
+++ R/fit_grf_surv.R
@@ -19,10 +19,12 @@
 #' @author bcjaeger
 
 # for debugging
-# data <- tar_read(data_sim)
-# fit_aorsf <- tar_read(fit_aorsf_sim)
+# data <- tar_read(data_melodem)
+# fit_orsf <- tar_read(fit_orsf)
 
-fit_grf_surv <- function(data, fit_orsf,
+fit_grf_surv <- function(data,
+                         trt_random,
+                         fit_orsf,
                          horizon = NULL,
                          target = "RMST") {
 
@@ -35,15 +37,25 @@ fit_grf_surv <- function(data, fit_orsf,
     horizon <- median(data_grf$Y)
   }
 
+  failure_times <- get_failure_times(data_grf)
+
+  W.hat <- if(trt_random == 'yes')
+    get_trt_prop(data$values$treatment)
+  else
+    get_trt_prop(fit_orsf)
+
   fit_grf <- causal_survival_forest(
     X = data_grf$X,
     Y = data_grf$Y,
     W = data_grf$W,
     D = data_grf$D,
-    W.hat = fit_orsf$pred_oobag[, 2],
+    W.hat = W.hat,
     horizon = horizon,
-    target = target
+    target = target,
+    failure.times = failure_times
   )
+
+  attr(fit_grf, 'failure_times') <- failure_times
 
   fit_grf
 
