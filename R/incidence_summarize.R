566a021a63cf72706ef37fb90cd6f6e392bbe551
bcjaeger <byron.jaeger@gmail.com>
2024-11-19 11:05:48 -0500

    fix incidence

--- R/incidence_summarize.R
+++ R/incidence_summarize.R
@@ -1,17 +1,13 @@
-
 incidence_summarize <- function(data_melodem){
 
   require(cmprsk)
 
-  trt <- Sys.getenv("melodem_data_trt_var")
-
   cuminc <- cuminc(ftime = data_melodem$values$time,
                    fstatus = data_melodem$values$status,
-                   group = data_melodem$values[[trt]])
+                   group = data_melodem$values$treatment)
 
   bind_rows(no = as_tibble(cuminc[[1]]),
             yes = as_tibble(cuminc[[2]]),
-            .id = 'temp') %>%
-    rename_with(.fn = function(x) trt, .cols = temp)
+            .id = 'treatment')
 
 }
